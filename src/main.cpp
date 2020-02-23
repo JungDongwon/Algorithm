424d8223a53ed2013224d1807aac648e1c22050e
정 동원 <dg52316@naver.com>
2020-02-23 13:10:23 +0000

    백준 1965

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 using namespace std;
-int a[1001][1001];
-int d[1001][1001];
-int main(){
+int a[1001];
+int d[1001]; // d[i] : i로 끝나는 수열 중 가장 긴 것
+int main() {
 	freopen("a.txt","r",stdin);
-	int i,j,n,m,t;
-	cin >> n >> m;
-	for(i=1;i<=n;i++){
-		for(j=1;j<=m;j++){
-			scanf("%1d",&t);
-			a[i][j]=t;
-			if ( i==1 || j==1 ) d[i][j]=t;
-		}
-	}	
-	for(i=2;i<=n;i++){
-		for(j=2;j<=m;j++){
-			if(a[i][j]==0) continue;
-			d[i][j] = min(d[i-1][j-1],min(d[i-1][j],d[i][j-1])) + 1;
+	int n;
+	cin >> n;
+	for(int i=1;i<=n;i++){
+		cin >> a[i];	
+	}
+	
+	for(int i=1;i<=n;i++){
+		d[i]=1;
+		for(int j=1;j<=i;j++){
+			if( a[j]<a[i] && d[i] < d[j] + 1 ){
+				d[i] = d[j] + 1;
+			}
 		}
 	}
 	int ans=0;
-	for(i=1;i<=n;i++){
-		for(j=1;j<=m;j++){
-			ans = max(ans,d[i][j]);
-		}
+	for(int i=1;i<=n;i++){
+		ans = max(ans,d[i]);	
 	}
-	cout << ans*ans << '\n';
+	cout << ans << '\n';
 } 
-
-	
