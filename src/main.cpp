0744c52f55dff458fbd471dd612034212da36c1f
정 동원 <dg52316@naver.com>
2020-02-25 03:38:19 +0000

    1697번 숨바꼭질 최단거리 문제 -> bfs

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,39 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 using namespace std;
+#define MAX 100000
 int m,n;
-int a[1001][1001];
-int d[1001][1001];
-int dx[] = {0,0,-1,1};
-int dy[] = {1,-1,0,0};
-queue<pair<int,int>> q;
-int cnt=0;
+int d[100001];
 int main() {
 	freopen("a.txt","r",stdin);
-	cin >> m >> n;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			int t;
-			cin >> t;
-			a[i][j] = t;
-			if(t==0) cnt++;
-			if(t==1){
-				q.push(make_pair(i,j));
-				d[i][j]=1;	
-			}
-		}
-	}
+	int n,k;
+	cin >> n >> k;
+	memset(d,-1,sizeof(d));
+	queue<int> q;
+	d[n]=0;
+	q.push(n);
 	while(!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
+		int c = q.front();
 		q.pop();
-		for(int k=0;k<4;k++){
-			int nx = x+dx[k];
-			int ny = y+dy[k];
-			if(nx>=0 && nx<n && ny>=0 && ny<m){
-				if(d[nx][ny]==0 && a[nx][ny]==0){
-					q.push(make_pair(nx,ny));
-					d[nx][ny] = d[x][y] + 1;
-				}
+		if(c-1>=0){
+			if(d[c-1]==-1){
+				q.push(c-1);
+				d[c-1]=d[c]+1;
+			}
+		}
+		if(c+1<=MAX){
+			if(d[c+1]==-1){
+				q.push(c+1);
+				d[c+1]=d[c]+1;
+			}
+		}
+		if(c*2<=MAX){
+			if(d[c*2]==-1){
+				q.push(c*2);
+				d[c*2]=d[c]+1;
 			}
 		}
 	}
-	int ans=0;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			if(d[i][j]==0 && a[i][j]!=-1){
-				cout << "-1" << '\n';
-				return 0;
-			}
-			ans = max(ans,d[i][j]);	
-		}
-	}
-	cout << ans-1 << '\n';
-	
+	cout << d[k] << '\n';
 } 
