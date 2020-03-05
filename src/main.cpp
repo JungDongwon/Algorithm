f42c25345b579abae98e11a3748e512071865656
정 동원 <dg52316@naver.com>
2020-03-05 11:00:08 +0000

    2206번 벽 부수고 이동하기 tuple을 이용해 (x,y,z)를 큐에  넣어 bfs을 이용하여 푸는 문제

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
-#include <cstring>
+#include <queue>
 #include <tuple>
-#include <deque>
 using namespace std;
-int a[101][101];
-int d[101][101];
+int a[1001][1001];
+int d[1001][1001][2];
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
 int main() {
 	freopen("a.txt","r",stdin);
 	int m,n;
-	cin >> m >> n;
+	cin >> n >> m;
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			scanf("%1d",&a[i][j]);	
 		}
 	}
-	memset(d,-1,sizeof(d));
-	deque<pair<int,int>> q;
-	d[1][1]=0;
-	q.push_back(make_pair(1,1));
+	queue<tuple<int,int,int>> q;
+	d[1][1][0] = 1;
+	q.push(make_tuple(1,1,0));
 	while(!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop_front();
+		int x,y,z;
+		tie(x,y,z) = q.front();
+		q.pop();
 		for(int k=0;k<4;k++){
 			int nx = x+dx[k];
 			int ny = y+dy[k];
 			if(nx>=1 && nx<=n && ny>=1 && ny<=m){
-				if(d[nx][ny]==-1){
-					if(a[nx][ny]==0){
-						q.push_front(make_pair(nx,ny));	
-						d[nx][ny] = d[x][y];
-					}
-					if(a[nx][ny]==1){
-						q.push_back(make_pair(nx,ny));	
-						d[nx][ny] = d[x][y] + 1;
-					}
+				if(a[nx][ny]==0 && d[nx][ny][z]==0){
+					d[nx][ny][z] = d[x][y][z]+1;
+					q.push(make_tuple(nx,ny,z));
+				}
+				if(z==0 && a[nx][ny]==1 && d[nx][ny][z+1]==0){
+					d[nx][ny][z+1] = d[x][y][z]+1;
+					q.push(make_tuple(nx,ny,z+1));
 				}
 				
 			}	
 		}
 	}
-/*	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			printf("%d ",d[i][j]);	
-		}
-		printf("\n");
-	}
-	*/
-	cout << d[n][m] << '\n';
+	if (d[n][m][0] != 0 && d[n][m][1] != 0) {
+        cout << min(d[n][m][0], d[n][m][1]);
+    } else if (d[n][m][0] != 0) {
+        cout << d[n][m][0];
+    } else if (d[n][m][1] != 0) {
+        cout << d[n][m][1];
+    } else {
+        cout << -1;
+    }
+    cout << '\n';
 } 
