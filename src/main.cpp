6ac0829eacad3540cc5a2287a2fb1102aea626a9
정 동원 <dg52316@naver.com>
2020-03-06 15:26:13 +0000

    13913번 숨바꼭질4 1697번 숨바꼭질 문제와 같은 문제 이 문제는 경로를 출력해야 하므로 print() 함수를 정의함. 재귀의 특성을 이용하여 역순으로 출력

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,53 @@
 #include <iostream>
-#include <string>
-#include <queue>
 #include <cstring>
+#include <queue>
 using namespace std;
-int water[51][51];
-int go[51][51];
-int dx[4] = {0,0,1,-1};
-int dy[4] = {1,-1,0,0};
+#define MAX 100000
+int m,n;
+int d[100001];
+int from[100001];
+
+void print(int n, int m){
+	if(n!=m){
+		print(n,from[m]);	
+	}
+	cout << m << " ";
+}
+
 int main() {
-	freopen("a.txt","r",stdin);
-	int r,c;
-	cin >> r >> c;
-	vector<string> a(r);
-	for(int i=0;i<r;i++){
-		cin >> a[i];	
-	}
-	memset(water,-1,sizeof(water));
-	memset(go,-1,sizeof(go));
-	queue<pair<int,int>> q;
-	int sx,sy,ex,ey;
-	for(int i=0;i<r;i++){
-		for(int j=0;j<c;j++){
-			if(a[i][j]=='*'){
-				q.push(make_pair(i,j));	
-				water[i][j]=0;
-			}
-			else if(a[i][j]=='S'){
-				sx = i;
-				sy = j;
-			}
-			else if(a[i][j]=='D'){
-				ex = i;
-				ey = j;
-			}
-		}
-	}
+	int n,k;
+	cin >> n >> k;
+	memset(d,-1,sizeof(d));
+	queue<int> q;
+	d[n]=0;
+	q.push(n);
 	while(!q.empty()){
-		int x = q.front().first; 
-		int y = q.front().second;
+		int c = q.front();
 		q.pop();
-		for(int k=0;k<4;k++){
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if( nx<0 || nx>=r || ny<0 || ny>=c ) continue;
-			if(water[nx][ny]!=-1) continue;
-			if( a[nx][ny]=='X' || a[nx][ny]=='D' ) continue;
-			water[nx][ny] = water[x][y] + 1;
-			q.push(make_pair(nx,ny));	
+		if(c-1>=0){
+			if(d[c-1]==-1){
+				q.push(c-1);
+				from[c-1]=c;
+				d[c-1]=d[c]+1;
+			}
+		}
+		if(c+1<=MAX){
+			if(d[c+1]==-1){
+				q.push(c+1);
+				from[c+1]=c;
+				d[c+1]=d[c]+1;
+			}
+		}
+		if(c*2<=MAX){
+			if(d[c*2]==-1){
+				q.push(c*2);
+				from[c*2]=c;
+				d[c*2]=d[c]+1;
+			}
 		}
 	}
-	q.push(make_pair(sx,sy));
-	go[sx][sy]=0;
-	while(!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for(int k=0;k<4;k++){
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if( nx<0 || nx>=r || ny<0 || ny>=c ) continue;
-			if(go[nx][ny]!=-1) continue;
-			if(a[nx][ny]=='X') continue;
-			if(water[nx][ny]!=-1 && go[x][y]+1>=water[nx][ny]) continue;
-			go[nx][ny] = go[x][y] + 1;
-			q.push(make_pair(nx,ny));
-		}
-	}
-	if(go[ex][ey]==-1) cout << "KAKTUS" << '\n';
-	else cout << go[ex][ey] << '\n';
+	cout << d[k] << '\n';
+	print(n,k);
+	cout << '\n';
+	
 } 
