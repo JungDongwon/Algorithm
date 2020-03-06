b38d2b447201dd744e4b803cffc2daaf0f6b235d
정 동원 <dg52316@naver.com>
2020-03-06 12:57:02 +0000

    3055번 탈출 물이 차오르는 시간과 고슴도치가 이동하는 시간을 따로따로 bfs로 구해야하는 문제.  bfs 2번만 이용하면됨

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,70 @@
 #include <iostream>
+#include <string>
 #include <queue>
-#include <tuple>
+#include <cstring>
 using namespace std;
-int a[1001][1001];
-int d[1001][1001][2];
+int water[51][51];
+int go[51][51];
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
 int main() {
 	freopen("a.txt","r",stdin);
-	int m,n;
-	cin >> n >> m;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			scanf("%1d",&a[i][j]);	
+	int r,c;
+	cin >> r >> c;
+	vector<string> a(r);
+	for(int i=0;i<r;i++){
+		cin >> a[i];	
+	}
+	memset(water,-1,sizeof(water));
+	memset(go,-1,sizeof(go));
+	queue<pair<int,int>> q;
+	int sx,sy,ex,ey;
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			if(a[i][j]=='*'){
+				q.push(make_pair(i,j));	
+				water[i][j]=0;
+			}
+			else if(a[i][j]=='S'){
+				sx = i;
+				sy = j;
+			}
+			else if(a[i][j]=='D'){
+				ex = i;
+				ey = j;
+			}
 		}
 	}
-	queue<tuple<int,int,int>> q;
-	d[1][1][0] = 1;
-	q.push(make_tuple(1,1,0));
 	while(!q.empty()){
-		int x,y,z;
-		tie(x,y,z) = q.front();
+		int x = q.front().first; 
+		int y = q.front().second;
 		q.pop();
 		for(int k=0;k<4;k++){
-			int nx = x+dx[k];
-			int ny = y+dy[k];
-			if(nx>=1 && nx<=n && ny>=1 && ny<=m){
-				if(a[nx][ny]==0 && d[nx][ny][z]==0){
-					d[nx][ny][z] = d[x][y][z]+1;
-					q.push(make_tuple(nx,ny,z));
-				}
-				if(z==0 && a[nx][ny]==1 && d[nx][ny][z+1]==0){
-					d[nx][ny][z+1] = d[x][y][z]+1;
-					q.push(make_tuple(nx,ny,z+1));
-				}
-				
-			}	
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if( nx<0 || nx>=r || ny<0 || ny>=c ) continue;
+			if(water[nx][ny]!=-1) continue;
+			if( a[nx][ny]=='X' || a[nx][ny]=='D' ) continue;
+			water[nx][ny] = water[x][y] + 1;
+			q.push(make_pair(nx,ny));	
 		}
 	}
-	if (d[n][m][0] != 0 && d[n][m][1] != 0) {
-        cout << min(d[n][m][0], d[n][m][1]);
-    } else if (d[n][m][0] != 0) {
-        cout << d[n][m][0];
-    } else if (d[n][m][1] != 0) {
-        cout << d[n][m][1];
-    } else {
-        cout << -1;
-    }
-    cout << '\n';
+	q.push(make_pair(sx,sy));
+	go[sx][sy]=0;
+	while(!q.empty()){
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for(int k=0;k<4;k++){
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if( nx<0 || nx>=r || ny<0 || ny>=c ) continue;
+			if(a[nx][ny]=='X') continue;
+			if(water[nx][ny]!=-1 && go[x][y]+1>=water[nx][ny]) continue;
+			go[nx][ny] = go[x][y] + 1;
+			q.push(make_pair(nx,ny));
+		}
+	}
+	if(go[ex][ey]==-1) cout << "KAKTUS" << '\n';
+	else cout << go[ex][ey] << '\n';
 } 
