2e58a54cf49f21e9c93ac39b38f82004ec166ad7
정 동원 <dg52316@naver.com>
2020-03-07 12:33:37 +0000

    2251번 물통 상태가 pair<int,int,int> 인 bfs 문제. 하지만 총 합이 정해져 있으므로 pair<int,int>로 전환하여 풀 수 있음. 하드코딩으로 풀긴 했는대 코드를 다듬을 필요가 있음.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,86 @@
 #include <iostream>
 #include <queue>
-#include <string>
-#include <map>
 using namespace std;
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
+bool d[201][201];
+bool ans[201];
 int main() {
 	freopen("a.txt","r",stdin);
-	int start=0;
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++){
-			int tmp;
-			cin >> tmp;
-			if(tmp==0) tmp=9;
-			start = start*10 + tmp;
-		}
-	}
-	queue<int> q;
-	map<int,int> d; // dist를 기록하는 저장소가 배열이 아니라 맵
-	
-	d[start]=0;
-	q.push(start);
+	int a,b,c;
+	cin >> a >> b >> c;
+	queue<pair<int,int>> q;
+	q.push(make_pair(0,0));
+	d[0][0] = true;
+	ans[c] = true;
 	while(!q.empty()){
-		int now_num = q.front();
-		string now = to_string(now_num);
+		int x,y,z;
+		x = q.front().first;
+		y = q.front().second;
+		z = c-x-y;
 		q.pop();
-		int z = now.find('9');
-		int x = z/3;
-		int y = z%3;
-		for(int k=0;k<4;k++){
-			int nx = x+dx[k];
-			int ny = y+dy[k];
-			if(nx>=0 && nx<3 && ny>=0 && ny<3){
-				string next = now;
-				swap(next[x*3+y],next[nx*3+ny]);
-				int num = stoi(next);
-				if(d.count(num)==0){
-					d[num] = d[now_num] + 1;
-					q.push(num);
-				}
-			}
+		int nx,ny,nz;
+		//x->y 물->용기
+		nx=x,ny=y,nz=z;
+		ny+=nx;
+		nx=0;
+		if(ny>b){ nx=ny-b; ny=b; } 
+		if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
+		}
+		//x->z
+		nx=x,ny=y,nz=z;
+		nz+=nx;
+		nx=0;
+		if(nz>c){ nx=nz-c; nz=c; } 
+			if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
+		}
+		//y->x
+		nx=x,ny=y,nz=z;
+		nx+=ny;
+		ny=0;
+		if(nx>a){ ny=nx-a; nx=a; } 
+			if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
+		}
+		//y->z
+		nx=x,ny=y,nz=z;
+		nz+=ny;
+		ny=0;
+		if(nz>c){ ny=nz-c; nz=c; } 
+			if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
+		}
+		//z->x
+		nx=x,ny=y,nz=z;
+		nx+=nz;
+		nz=0;
+		if(nx>a){ nz=nx-a; nx=a; } 
+		if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
+		}
+		//z->y
+		nx=x,ny=y,nz=z;
+		nx+=ny;
+		ny=0;
+		if(ny>b){ nz=ny-b; ny=b; } 
+			if(!d[nx][ny]){
+			q.push(make_pair(nx,ny));
+			d[nx][ny]=true;
+			if(nx==0) ans[nz]=true;
 		}
 	}
-	if(d.count(123456789)==0) cout << -1 << '\n';
-	else cout << d[123456789] <<'\n';
+	for(int i=0;i<=200;i++){
+		if(ans[i]) cout << i << " ";
+	}
+	cout << '\n';
 } 
