3c85ec1734f768614f7219056f5c5cacd6852dd6
정 동원 <dg52316@naver.com>
2020-03-13 04:27:07 +0000

    4991번  로봇청소기 더러운 곳을 청소하는 순서를 정해놓고 이동 횟수의 최솟값을 구하는 문제. next_permutation을 이용하여 갈 수 있는 조합을 구한다음 bfs로 경로의 길이를 구한다음 최솟값을 구하는 문제. 문제 로직은 간단하지만 구현하기가 쉽지 않았다. bfs를 함수로 뺄 때 리턴값과 파라미터를 어떤 것으로 할지가 중요.

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,80 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <string>
-#include <deque>
-#include <tuple>
+#include <algorithm>
 using namespace std;
 int dx[4] = {0,0,-1,1};
 int dy[4] = {-1,1,0,0};
-vector<vector<int>> bfs(vector<string> &a, int x, int y){
+vector<vector<int>> bfs(vector<string> &a, int sx, int sy){
 	int n = a.size();
 	int m = a[0].size();
-	vector<vector<int>> d(n,vector<int>(m));
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){	
-			d[i][j]=-1;
-		}
-	}
-	deque<pair<int,int>> q;
-	q.push_back(make_pair(x,y));
-	d[x][y]=0;
+	vector<vector<int>> dist(n,vector<int>(m,-1));
+	queue<pair<int,int>> q;
+	q.push(make_pair(sx,sy));
+	dist[sx][sy]=0;
 	while(!q.empty()){
-		tie(x,y) = q.front(); q.pop_front();
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
 		for(int k=0;k<4;k++){
 			int nx = x+dx[k];
 			int ny = y+dy[k];
-			if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-			if(d[nx][ny]!=-1) continue;
-			if(a[nx][ny]=='*') continue;
-			if(a[nx][ny]=='#'){
-				d[nx][ny] = d[x][y]+1;
-				q.push_back(make_pair(nx,ny));
-			}
-			else{
-				d[nx][ny] = d[x][y];
-				q.push_front(make_pair(nx,ny));
-			}
-		}
-	}
-	return d;
-}
-
-int main() {
-	freopen("a.txt","r",stdin);
-	int t;
-	cin >> t;
-	while(t--){
-		int h,w;
-		cin >> h >> w;
-		vector<string> a(h+2);
-		for(int i=1;i<=h;i++){
-			cin >> a[i];
-			a[i] = "." + a[i] + ".";
-		}
-		h+=2; w+=2;
-		for(int i=0;i<w;i++){
-			a[0]+=".";
-			a[h-1]+=".";
-		}
-		int x1=-1,x2=-1,y1=-1,y2=-1;
-		for(int i=0;i<h;i++){
-			for(int j=0;j<w;j++){
-				if(a[i][j]=='$'){
-					if(x1==-1){
-						x1=i;
-						y1=j;
-					}
-					else{
-						x2=i;
-						y2=j;
-					}
+			if(nx>=0 && nx<n && ny>=0 && ny<m){
+				if(a[nx][ny]!='x' && dist[nx][ny]==-1){
+					q.push(make_pair(nx,ny));
+					dist[nx][ny] = dist[x][y] + 1;
 				}
 			}
 		}
-		vector<vector<int>> d0 = bfs(a,0,0);
-		vector<vector<int>> d1 = bfs(a,x1,y1);
-		vector<vector<int>> d2 = bfs(a,x2,y2);
-		int ans = h*w;
-		for(int i=0;i<h;i++){
-			for(int j=0;j<w;j++){
-				if(a[i][j]=='*') continue;
-				int cur = d0[i][j] + d1[i][j] + d2[i][j];
-				if(a[i][j]=='#') cur-=2;
-				if(ans>cur) ans=cur;
+	}
+	return dist;
+}
+int main() {
+	freopen("a.txt","r",stdin);
+	while(true){
+		int n,m;
+		cin >> m >> n;
+		if(n==0 && m==0) break;
+		vector<string> a(n);
+		for(int i=0;i<n;i++){
+			cin >> a[i];	
+		}
+		vector<pair<int,int>> b(1); // 한자리는 시작점을 위해서..
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(a[i][j]=='o') b[0] = make_pair(i,j);
+				else if(a[i][j]=='*') b.push_back(make_pair(i,j));
 			}
 		}
+		int l=b.size();
+		vector<vector<int>> d(l,vector<int>(l));
+		bool ok = true;
+		for(int i=0;i<l;i++){
+			auto dist = bfs(a,b[i].first,b[i].second);
+			for(int j=0;j<l;j++){
+				d[i][j] = dist[b[j].first][b[j].second];
+				if(d[i][j]==-1){
+					ok=false;	
+				}
+			}
+		}
+		if(ok==false){
+			cout << -1 << '\n';
+			continue;
+		}
+		vector<int> p(l-1);
+		for(int i=0;i<l-1;i++){
+			p[i]=i+1;	
+		}
+		int ans=-1;
+		do{
+			int now = d[0][p[0]];
+			for(int i=0;i<l-2;i++){
+				now += d[p[i]][p[i+1]];	
+			}
+			if(ans==-1 || ans>now) ans=now;
+		}while(next_permutation(p.begin(),p.end()));
 		cout << ans << '\n';
 	}
 } 
