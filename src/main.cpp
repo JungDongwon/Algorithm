b2e69c3ed2ae0dc5fcbe579a909fa867c218cc9d
정 동원 <dg52316@naver.com>
2020-03-14 08:39:56 +0000

    2573번 빙산 bfs 와 시뮬레이션 문제가 합쳐진 문제. 메커니즘은 어렵지 않지만 짜야할 함수가 많아서 실수하기 쉬운 문제. 다행이 한번에 성공

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <string>
 using namespace std;
-int a[51][51];
 int n,m;
 int dx[4] = {0,0,-1,1};
 int dy[4] = {-1,1,0,0};
-int bfs(int i, int j){
+int bfs(vector<vector<int>> a, int i, int j){
 	queue<pair<int,int>> q;
 	vector<vector<bool>> d(n,vector<bool>(m,false));
-	vector<vector<int>> cnt(n,vector<int>(m,-1));
 	q.push(make_pair(i,j));
 	d[i][j]=true;
-	cnt[i][j]=0;
+	int count=1;
 	while(!q.empty()){
 		int x = q.front().first;
 		int y = q.front().second;
@@ -22,41 +20,81 @@ int bfs(int i, int j){
 			int nx = x + dx[l];
 			int ny = y + dy[l];
 			if(nx>=0 && nx<n && ny>=0 && ny<m){
-				if(d[nx][ny]==false && a[nx][ny]==1){
+				if(d[nx][ny]==false && a[nx][ny]!=0){
 					q.push(make_pair(nx,ny));
 					d[nx][ny] = true;
-					cnt[nx][ny] = cnt[x][y]+1;
+					count++;
 				}
 			}
 		}
 	}
-	int cur=0;
+	return count;
+}
+bool seperated(vector<vector<int>> a){
+	int cnt=0,sx,sy;
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
-				cur = max(cur,cnt[i][j]);
+			if(a[i][j]!=0){
+				cnt++;	
+				if(cnt==1){
+					sx=i; 
+					sy=j;
+				}
+			}
 		}
 	}
-	return cur;
+	int cur = bfs(a,sx,sy);
+	if(cnt==cur) return false;
+	else return true;
+}
+vector<vector<int>> simul(vector<vector<int>> a){
+	int count=0;
+	vector<vector<int>> b = a;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(a[i][j]!=0){
+				count=0;
+				for(int k=0;k<4;k++){
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					if(nx>=0 && nx<n && ny>=0 && ny<m){
+						if(a[nx][ny]==0) count++;	
+					}
+				}
+				if(a[i][j]>=count) b[i][j]-=count;
+				else b[i][j]=0;
+			}
+		}
+	}
+	return b;
+}
+bool allmelt(vector<vector<int>> a){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(a[i][j]!=0) return false;	
+		}
+	}
+	return true;
 }
 int main() {
 	freopen("a.txt","r",stdin);
 	cin >> n >> m;
+	static vector<vector<int>> a(n,vector<int>(m,0));
 	for(int i=0;i<n;i++){
-		string s;
-		cin >> s;
 		for(int j=0;j<m;j++){
-			if(s[j]=='W') a[i][j]=0;
-			else a[i][j]=1;
+			cin >> a[i][j];	
 		}
 	}
-	int ans=0;
-	for(int i=0;i<m;i++){
-		for(int j=0;j<m;j++){
-			if(a[i][j]==1){
-				int cur = bfs(i,j);
-				ans = max(cur,ans);
-			}
+	int year=0;
+	while(true){
+		if(allmelt(a)){
+			cout << 0 << '\n';
+			return 0;
 		}
-	}	
-	cout << ans << '\n';
+		if(seperated(a)) break;
+		a = simul(a);	
+		year++;	
+	}
+	cout << year << '\n';
+	return 0;
 }
