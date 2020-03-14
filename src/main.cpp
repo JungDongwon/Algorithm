951bcab05ba75a91da880f2c8a1ca0a6b866bab2
정 동원 <dg52316@naver.com>
2020-03-14 07:20:35 +0000

    2589번 보물섬 bfs를 통해 구한 최단경로 중 가장 긴 길이를 구하는 문제. 모든 노드에서 bfs를 이용하여 가장 긴 경로의 길이를 구하고 가장 긴 길이를 출력하면 됨.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <cstring>
+#include <string>
 using namespace std;
 int a[51][51];
-bool d[51][51];
 int n,m;
 int dx[4] = {0,0,-1,1};
 int dy[4] = {-1,1,0,0};
-void bfs(int i, int j){
+int bfs(int i, int j){
 	queue<pair<int,int>> q;
+	vector<vector<bool>> d(n,vector<bool>(m,false));
+	vector<vector<int>> cnt(n,vector<int>(m,-1));
 	q.push(make_pair(i,j));
 	d[i][j]=true;
+	cnt[i][j]=0;
 	while(!q.empty()){
 		int x = q.front().first;
 		int y = q.front().second;
@@ -23,34 +25,38 @@ void bfs(int i, int j){
 				if(d[nx][ny]==false && a[nx][ny]==1){
 					q.push(make_pair(nx,ny));
 					d[nx][ny] = true;
+					cnt[nx][ny] = cnt[x][y]+1;
 				}
 			}
 		}
 	}
+	int cur=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+				cur = max(cur,cnt[i][j]);
+		}
+	}
+	return cur;
 }
 int main() {
 	freopen("a.txt","r",stdin);
-	int t;
-	cin >> t;
-	while(t--){
-		memset(a,0,sizeof(a));
-		memset(d,false,sizeof(d));
-		int k;
-		cin >> n >> m >> k;
-		for(int i=0;i<k;i++){
-			int x,y;
-			cin >> x >> y;
-			a[x][y]=1;
+	cin >> n >> m;
+	for(int i=0;i<n;i++){
+		string s;
+		cin >> s;
+		for(int j=0;j<m;j++){
+			if(s[j]=='W') a[i][j]=0;
+			else a[i][j]=1;
 		}
-		int ans=0;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(a[i][j]==1 && d[i][j]==false){
-					bfs(i,j);
-					ans++;
-				}
+	}
+	int ans=0;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			if(a[i][j]==1){
+				int cur = bfs(i,j);
+				ans = max(cur,ans);
 			}
 		}
-		cout << ans << '\n';
-	}
-} 
+	}	
+	cout << ans << '\n';
+}
