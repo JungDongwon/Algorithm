102f33622a57ed05502140fc315b14b968edf405
정 동원 <dg52316@naver.com>
2020-03-15 13:21:45 +0000

    7569번 토마토 일반적인 bfs 문제인대 서치를 6방향(위,아래 포함)을 하는 문제 3차원배열을 사용하고 dx,dy,dx를 이용하여 6방향을 만듬.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,61 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
 #include <cstring>
 using namespace std;
-int a[101][101];
-int d[101][101];
-int c[101];
-int n;
-void dfs(int x){
-	for(int i=0;i<n;i++){
-		if(a[x][i]==1 && c[i]==0){
-			c[i]=1;
-			dfs(i);
-		}
-	}
-}
+int a[101][101][101];
+int d[101][101][101];
+int dx[6] = {0,0,1,-1,0,0};
+int dy[6] = {1,-1,0,0,0,0};
+int dz[6] = {0,0,0,0,1,-1};
+int cntwall=0;
 int main() {
 	freopen("a.txt","r",stdin);
-	cin >> n;
-	for(int i=0;i<n;i++){
+	int m,n,h;
+	cin >> m >> n >> h;
+	queue<tuple<int,int,int>> q;
+	memset(d,-1,sizeof(d));
+	for(int i=0;i<h;i++){
 		for(int j=0;j<n;j++){
-			cin >> a[i][j];	
+			for(int k=0;k<m;k++){
+				int t;
+				cin >> t;
+				a[j][k][i]=t;
+				if(t==1){
+					q.push(make_tuple(j,k,i));
+					d[j][k][i]=0;
+				}
+				if(t==-1) cntwall++;
+			}
 		}
 	}
-	for(int i=0;i<n;i++){
-		memset(c,0,sizeof(c));
-		dfs(i);
-		for(int j=0;j<n;j++){
-			if(c[j]==1) d[i][j]=1;	
+	while(!q.empty()){
+		int x,y,z;
+		tie(x,y,z)=q.front();
+		q.pop();
+		for(int k=0;k<6;k++){
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			int nz = z + dz[k];
+			if(nx>=0 && nx<n && ny>=0 && ny<m && nz>=0 && nz<h){
+				if(a[nx][ny][nz]==0 && d[nx][ny][nz]==-1){
+					q.push(make_tuple(nx,ny,nz));
+					d[nx][ny][nz]=d[x][y][z]+1;
+				}
+			}
 		}
 	}
-	for(int i=0;i<n;i++){
+	int ans=0,curwall=0;
+	for(int i=0;i<h;i++){
 		for(int j=0;j<n;j++){
-			cout << d[i][j] << " ";	
+			for(int k=0;k<m;k++){
+				ans=max(d[j][k][i],ans);	
+				if(d[j][k][i]==-1) curwall++;
+			}
 		}
-		cout << '\n';
 	}
+	if(cntwall==curwall) cout << ans << '\n';
+	else cout << -1 << '\n';
+	
 }
