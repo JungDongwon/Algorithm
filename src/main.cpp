8bf65d9183f1cb2f5ba13381d6382afffcf0d08d
정 동원 <dg52316@naver.com>
2020-03-16 14:00:46 +0000

    2644번 촌수계산 전형적인 bfs 기본문제.

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
 #include <cstring>
 using namespace std;
-int a[101][101][101];
-int d[101][101][101];
-int dx[6] = {0,0,1,-1,0,0};
-int dy[6] = {1,-1,0,0,0,0};
-int dz[6] = {0,0,0,0,1,-1};
-int cntwall=0;
+int a[101][101];
+int d[101];
 int main() {
 	freopen("a.txt","r",stdin);
-	int m,n,h;
-	cin >> m >> n >> h;
-	queue<tuple<int,int,int>> q;
+	int n,m,sx,sy;
+	cin >> n >> sx >> sy >> m;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin >> x >> y;
+		a[x][y]=1;
+		a[y][x]=1;
+	}
+	queue<int> q;
 	memset(d,-1,sizeof(d));
-	for(int i=0;i<h;i++){
-		for(int j=0;j<n;j++){
-			for(int k=0;k<m;k++){
-				int t;
-				cin >> t;
-				a[j][k][i]=t;
-				if(t==1){
-					q.push(make_tuple(j,k,i));
-					d[j][k][i]=0;
-				}
-				if(t==-1) cntwall++;
-			}
-		}
-	}
+	q.push(sx);
+	d[sx]=0;
 	while(!q.empty()){
-		int x,y,z;
-		tie(x,y,z)=q.front();
+		int c = q.front();
 		q.pop();
-		for(int k=0;k<6;k++){
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			int nz = z + dz[k];
-			if(nx>=0 && nx<n && ny>=0 && ny<m && nz>=0 && nz<h){
-				if(a[nx][ny][nz]==0 && d[nx][ny][nz]==-1){
-					q.push(make_tuple(nx,ny,nz));
-					d[nx][ny][nz]=d[x][y][z]+1;
-				}
+		for(int i=1;i<=n;i++){
+			if(a[c][i]==1 && d[i]==-1){
+				q.push(i);
+				d[i]=d[c]+1;
 			}
 		}
 	}
-	int ans=0,curwall=0;
-	for(int i=0;i<h;i++){
-		for(int j=0;j<n;j++){
-			for(int k=0;k<m;k++){
-				ans=max(d[j][k][i],ans);	
-				if(d[j][k][i]==-1) curwall++;
-			}
-		}
-	}
-	if(cntwall==curwall) cout << ans << '\n';
-	else cout << -1 << '\n';
-	
+	 cout << d[sy] << '\n';
 }
