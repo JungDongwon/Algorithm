3f253d3673acfb8aba7306ab6a120f9a3531eb96
정 동원 <dg52316@naver.com>
2020-03-17 10:30:11 +0000

    6593번 상범빌딩 토마토 문제랑 비슷하게 4방향+위,아래 -> 6방향으로 bfs를 적용하는 문제.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <tuple>
 #include <cstring>
 using namespace std;
-int a[101][101];
-int d[101];
+char a[31][31][31];
+int d[31][31][31];
+int dx[6] = {0,0,1,-1,0,0};
+int dy[6] = {1,-1,0,0,0,0};
+int dz[6] = {0,0,0,0,1,-1};
 int main() {
 	freopen("a.txt","r",stdin);
-	int n,m,sx,sy;
-	cin >> n >> sx >> sy >> m;
-	for(int i=0;i<m;i++){
-		int x,y;
-		cin >> x >> y;
-		a[x][y]=1;
-		a[y][x]=1;
-	}
-	queue<int> q;
-	memset(d,-1,sizeof(d));
-	q.push(sx);
-	d[sx]=0;
-	while(!q.empty()){
-		int c = q.front();
-		q.pop();
-		for(int i=1;i<=n;i++){
-			if(a[c][i]==1 && d[i]==-1){
-				q.push(i);
-				d[i]=d[c]+1;
+	while(1){
+		int m,n,h,ax,ay,az;
+		cin >> h >> n >> m;
+		if(h==0 && m==0 && n==0) return 0;
+		queue<tuple<int,int,int>> q;
+		memset(d,-1,sizeof(d));
+		for(int i=0;i<h;i++){
+			for(int j=0;j<n;j++){
+				for(int k=0;k<m;k++){
+					char t;
+					cin >> t;
+					a[j][k][i]=t;
+					if(t=='S'){
+						q.push(make_tuple(j,k,i));
+						d[j][k][i]=0;
+					}
+					if(t=='E'){
+						ax=j;
+						ay=k;
+						az=i;
+					}
+				}
 			}
 		}
+		while(!q.empty()){
+			int x,y,z;
+			tie(x,y,z)=q.front();
+			q.pop();
+			for(int k=0;k<6;k++){
+				int nx = x + dx[k];
+				int ny = y + dy[k];
+				int nz = z + dz[k];
+				if(nx>=0 && nx<n && ny>=0 && ny<m && nz>=0 && nz<h){
+					if(a[nx][ny][nz]!='#' && d[nx][ny][nz]==-1){
+						q.push(make_tuple(nx,ny,nz));
+						d[nx][ny][nz]=d[x][y][z]+1;
+					}
+				}
+			}
+		}
+		int ans=d[ax][ay][az];
+		if(ans!=-1) printf("Escaped in %d minutes(s).\n",ans);
+		else printf("Trapped!\n");
 	}
-	 cout << d[sy] << '\n';
+	
+	
 }
