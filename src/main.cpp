9688bac6cc9dd3b5a64c467868d847dd76123707
정 동원 <dg52316@naver.com>
2020-03-20 05:31:13 +0000

    11559번 Puyo Puyo bfs문제와 시뮬레이션 문제가 합쳐진 문제. 구현해야할 기능이 좀 있어서 실수하기 쉬운문제. 처음에는 연쇄의 개념이 헷갈려서 오답.

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,87 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
-#include <cstring>
+#include <string>
 using namespace std;
-char a[31][31][31];
-int d[31][31][31];
-int dx[6] = {0,0,1,-1,0,0};
-int dy[6] = {1,-1,0,0,0,0};
-int dz[6] = {0,0,0,0,1,-1};
+char a[12][6];
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
+int ans=0;
+bool explode(int i, int j){
+	queue<pair<int,int>> q;
+	vector<vector<bool>> d(12,vector<bool>(6));
+	int cnt=0;
+	q.push(make_pair(i,j));
+	d[i][j]=true; cnt++;
+	while(!q.empty()){
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for(int k=0;k<4;k++){
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if(nx>=0 && nx<12 && ny>=0 && ny<6){
+				if(a[nx][ny]==a[x][y] && d[nx][ny]==false){
+					q.push(make_pair(nx,ny));
+					d[nx][ny]=true;
+					cnt++;
+				}
+			}
+		}
+	}
+	if(cnt>=4){
+		for(int i=0;i<12;i++){
+			for(int j=0;j<6;j++){
+				if(d[i][j]==true) a[i][j]='.';	
+			}
+		}
+		return true;
+	}
+	return false;
+}
+void simul(){
+	for(int j=0;j<6;j++){
+		string s = "";
+		for(int i=11;i>=0;i--){
+			if(a[i][j]=='.') continue;
+			s+=a[i][j];
+			a[i][j]='.';
+		}
+		for(int i=0;i<s.size();i++){
+			a[11-i][j] = s[i];
+		}
+	}
+}
 int main() {
 	freopen("a.txt","r",stdin);
-	while(1){
-		int m,n,h,ax,ay,az;
-		cin >> h >> n >> m;
-		if(h==0 && m==0 && n==0) return 0;
-		queue<tuple<int,int,int>> q;
-		memset(d,-1,sizeof(d));
-		for(int i=0;i<h;i++){
-			for(int j=0;j<n;j++){
-				for(int k=0;k<m;k++){
-					char t;
-					cin >> t;
-					a[j][k][i]=t;
-					if(t=='S'){
-						q.push(make_tuple(j,k,i));
-						d[j][k][i]=0;
-					}
-					if(t=='E'){
-						ax=j;
-						ay=k;
-						az=i;
-					}
-				}
-			}
+	for(int i=0;i<12;i++){
+		for(int j=0;j<6;j++){
+			cin >> a[i][j];
 		}
-		while(!q.empty()){
-			int x,y,z;
-			tie(x,y,z)=q.front();
-			q.pop();
-			for(int k=0;k<6;k++){
-				int nx = x + dx[k];
-				int ny = y + dy[k];
-				int nz = z + dz[k];
-				if(nx>=0 && nx<n && ny>=0 && ny<m && nz>=0 && nz<h){
-					if(a[nx][ny][nz]!='#' && d[nx][ny][nz]==-1){
-						q.push(make_tuple(nx,ny,nz));
-						d[nx][ny][nz]=d[x][y][z]+1;
-					}
-				}
-			}
-		}
-		int ans=d[ax][ay][az];
-		if(ans!=-1) printf("Escaped in %d minutes(s).\n",ans);
-		else printf("Trapped!\n");
 	}
-	
-	
+	while(true){
+		bool cur = false;
+		for(int i=0;i<12;i++){
+			for(int j=0;j<6;j++){
+				if(a[i][j]!='.') {
+					if(explode(i,j)){
+						cur=true;
+					}
+				}
+			}
+		}
+		if(cur) ans++;
+		else{
+			cout << ans << '\n';
+			return 0;
+		}
+		simul();		
+	/*	for(int i=0;i<12;i++){
+			for(int j=0;j<6;j++){
+				cout << a[i][j];
+			}
+			cout << '\n';
+		}
+		cout << '\n';*/
+	}
 }
