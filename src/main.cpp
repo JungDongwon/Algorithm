ae35efbec1eef3e9cd6490d99fb1b95adcafe394
정 동원 <dg52316@naver.com>
2020-03-22 08:48:15 +0000

    1107번 리모컨 브루트포스 1부터 100만 까지의 채널을 누르는 경우 모두 구해서 최솟값을 출력하는 문제. 최소가 되려면 무조건 채널을 숫자로 누른다음에 +,- 버튼을 눌러야함. 또한 + 또는 - 버튼만 사용해야 최소가능. 이 규칙만 알아낸다면 어렵지 않은 문제.

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <string>
+#define MAX 1000000
 using namespace std;
-char a[12][6];
-int dx[4] = {0,0,-1,1};
-int dy[4] = {-1,1,0,0};
-int ans=0;
-bool explode(int i, int j){
-	queue<pair<int,int>> q;
-	vector<vector<bool>> d(12,vector<bool>(6));
-	int cnt=0;
-	q.push(make_pair(i,j));
-	d[i][j]=true; cnt++;
-	while(!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for(int k=0;k<4;k++){
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if(nx>=0 && nx<12 && ny>=0 && ny<6){
-				if(a[nx][ny]==a[x][y] && d[nx][ny]==false){
-					q.push(make_pair(nx,ny));
-					d[nx][ny]=true;
-					cnt++;
-				}
-			}
-		}
+int n,m;
+int broken[10] = {false};
+int possible(int c){
+	if(c==0){
+		if(broken[0]) return 0;
+		else return 1;
 	}
-	if(cnt>=4){
-		for(int i=0;i<12;i++){
-			for(int j=0;j<6;j++){
-				if(d[i][j]==true) a[i][j]='.';	
-			}
-		}
-		return true;
+	int len=0;
+	while(c>0){
+		int t = c%10;
+		if(broken[t]) return 0;	
+		len++;
+		c/=10;
 	}
-	return false;
-}
-void simul(){
-	for(int j=0;j<6;j++){
-		string s = "";
-		for(int i=11;i>=0;i--){
-			if(a[i][j]=='.') continue;
-			s+=a[i][j];
-			a[i][j]='.';
-		}
-		for(int i=0;i<s.size();i++){
-			a[11-i][j] = s[i];
-		}
-	}
+	return len;
 }
 int main() {
 	freopen("a.txt","r",stdin);
-	for(int i=0;i<12;i++){
-		for(int j=0;j<6;j++){
-			cin >> a[i][j];
-		}
+	cin >> n >> m;
+	for(int i=0;i<m;i++){
+		int t;
+		cin >> t;
+		broken[t]=true;
 	}
-	while(true){
-		bool cur = false;
-		for(int i=0;i<12;i++){
-			for(int j=0;j<6;j++){
-				if(a[i][j]!='.') {
-					if(explode(i,j)){
-						cur=true;
-					}
-				}
-			}
-		}
-		if(cur) ans++;
-		else{
-			cout << ans << '\n';
-			return 0;
-		}
-		simul();		
-	/*	for(int i=0;i<12;i++){
-			for(int j=0;j<6;j++){
-				cout << a[i][j];
-			}
-			cout << '\n';
-		}
-		cout << '\n';*/
+	int ans=abs(n-100);
+	for(int i=0;i<=MAX;i++){
+		int c = i;
+		int r = possible(c);
+		if(r==0) continue;
+		ans = min(r+abs(n-c),ans);
 	}
+	cout << ans <<'\n';
 }
