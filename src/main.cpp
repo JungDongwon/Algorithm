54258fc28b1bcf4e9052cae9ccfe82c309e76e63
정 동원 <dg52316@naver.com>
2020-03-23 14:49:43 +0000

    AtCoder ABC 159 d. 시간초과 주의. O(n)으로 풀어야함 큰 수를 다루므로 long long 사용해야함

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-#define MAX 1000000
+#include <vector>
 using namespace std;
-int n,m;
-int broken[10] = {false};
-int possible(int c){
-	if(c==0){
-		if(broken[0]) return 0;
-		else return 1;
-	}
-	int len=0;
-	while(c>0){
-		int t = c%10;
-		if(broken[t]) return 0;	
-		len++;
-		c/=10;
-	}
-	return len;
-}
+long long a[200001];
+long long b[200001];
 int main() {
 	freopen("a.txt","r",stdin);
-	cin >> n >> m;
-	for(int i=0;i<m;i++){
+	long long n;
+	cin >> n;
+	for(int i=0;i<n;i++){
 		int t;
 		cin >> t;
-		broken[t]=true;
+		a[i]=t;
+		b[t]+=1;
 	}
-	int ans=abs(n-100);
-	for(int i=0;i<=MAX;i++){
-		int c = i;
-		int r = possible(c);
-		if(r==0) continue;
-		ans = min(r+abs(n-c),ans);
+	long long total=0;
+	for(int i=1;i<=n;i++){
+		total+=b[i]*(b[i]-1)/2;		
 	}
-	cout << ans <<'\n';
+	
+	for(int i=0;i<n;i++){
+		cout << total-(b[a[i]]-1) << '\n';	
+	}
 }
