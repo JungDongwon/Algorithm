2c682a6842b9a5e961da001c95c92c282fb6e7b0
정 동원 <dg52316@naver.com>
2020-03-25 12:59:06 +0000

    1931번 회의실배정 그리디 회의가 끝나는 시간을 기준으로 다음 회의를 찾아나가면 가장 많은 회의를 할 수 있음.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool ss(pair<int,int> a, pair<int,int> b){
+	if(a.second==b.second){
+		return a.first<b.first;	
+	}
+	return a.second<b.second;
+}
+
 int main() {
 	freopen("a.txt","r",stdin);
-	int n,k;
-	cin >> n >> k;
-	vector<int> a(n);
+	int n;
+	cin >> n;
+	vector<pair<int,int>> a(n);
 	for(int i=0;i<n;i++){
-		cin >> a[i];	
+		cin >> a[i].first >> a[i].second;
 	}
 	int ans=0;
-	for(int i=n-1;i>=0;i--){
-		if(k<a[i]) continue;
-		else{
-			while(1){
-				k-=a[i];
-				ans++;
-				if(k<a[i]) break;
-			}
+	int k=0; //끝나는 시간
+	sort(a.begin(),a.end(),ss);
+	for(int i=0;i<n;i++){
+		if(k<=a[i].first){ // sort 가 되있으므로 그냥 순서대로 보면서 가장 처음 만족하는 n이 최소가 될 수 있는 n이다.
+			k=a[i].second;
+			ans++;
 		}
 	}
-	cout << ans <<'\n';
+	
+	cout << ans << '\n';
 }
 	
