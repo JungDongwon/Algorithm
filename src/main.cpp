4b64aebc61da31870c94fef9703097a8fee5cb25
정 동원 <dg52316@naver.com>
2020-04-01 11:36:37 +0000

    2437번 저울 greedy O(n)으로 간단히 풀 수 있는 문제 가능한 추의 무게를 순차적으로 받고 표현되지 못하는 무게가 무엇인지 생각한다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
 	freopen("a.txt","r",stdin);
-	int t;
-	cin >> t;
-	while(t--){
-		int n,k;
-		cin >> n >> k;
-		int i,sum=0;
-		for(i=1;i<n;i++){
-			sum+=i;
-			if(sum>=k) break;
-		}
-		int b1 = i;
-		int b2 = i-(sum-k+1);
-		string s(n,'a');
-		s[n-b1-1]='b';
-		s[n-b2-1]='b';
-		cout << s << '\n';
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i=0;i<n;i++){
+		cin >> a[i];
 	}
+	sort(a.begin(),a.end());
+	//1 1 2 3 6 7 30
+	int sum=0;
+	for(int i=0;i<n;i++){
+		if(sum+2<=a[i]) break;
+		sum+=a[i];
+	}
+	cout << sum+1 <<'\n';
+	return 0;
 }
 	
