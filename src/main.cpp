0bfad0f3b7a12d9d2c48abd048603ac8b3d2aed5
정 동원 <dg52316@naver.com>
2020-07-01 05:48:50 +0000

    sliding window algorithm. dp로 풀면 메모리 초과, 전 단계와 현재 단계의 배열만 가지고 풀어야 한다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
+
 int main() {
 	freopen("a.txt","r",stdin);
-	int c;
-	int m,n;
-	int ans=0;
-	cin >> n >> m;
-	vector<int> a(n);
-	for(int i=0;i<n;i++){
-		cin >> a[i];	
-	}
-	int s = 0, l = 0, r = 0;
-	while(1){
-		if(s > m) {
-			s-=a[l++];
+	int n,i;
+	int a[3];
+	int prevd[3], curd[3];
+	int prevd1[3], curd1[3];
+	cin >> n;
+	cin >> prevd[0] >> prevd[1] >> prevd[2];
+	prevd1[0]=prevd[0];
+	prevd1[1]=prevd[1];
+	prevd1[2]=prevd[2];
+	for(int i=1;i<n;i++){
+		for(int j=0;j<3;j++){
+			cin >> a[j];
+			if(j==0){
+				curd[j] = max(prevd[0],prevd[1]) + a[j];	
+				curd1[j] = min(prevd1[0],prevd1[1]) + a[j];	
+			}
+			else if(j==2){
+				curd[j] = max(prevd[1],prevd[2]) + a[j];	
+				curd1[j] = min(prevd1[1],prevd1[2]) + a[j];	
+			}
+			else{
+				curd[j] = max(prevd[2],max(prevd[0],prevd[1])) + a[j];	
+				curd1[j] = min(prevd1[2],min(prevd1[0],prevd1[1])) + a[j];	
+			}
 		}
-		else if(r == n) break;
-		else s += a[r++];
-		if(s == m){
-			ans++;
+		for(int j=0;j<3;j++){
+			prevd[j] = curd[j];
+			prevd1[j] = curd1[j];
 		}
 	}
-	cout << ans << '\n';
+	printf("%d %d\n",max(prevd[0],max(prevd[1],prevd[2])), min(prevd1[0],min(prevd1[1],prevd1[2])));
 }
