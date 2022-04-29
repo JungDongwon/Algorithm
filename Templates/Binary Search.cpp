6f08828d51b3cc2257e4ab4399ad2a95d521af0c
Jung Dongwon <dg52316@naver.com>
2022-04-29 15:14:22 +0900

    Update Binary Search.cpp

--- Templates/Binary Search.cpp
+++ Templates/Binary Search.cpp
@@ -1,4 +1,8 @@
-int binarySearch(vector<int>& nums, int target){
+// binarySearch1: 가장 기본적인 형태, 하나의 element만 접근하여 정답을 얻을 수 있는 경우 사용
+// binarySearch2: 두개의 element(가운데, 오른쪽)에 접근하여 정답을 얻을 수 있는 경우 사용. Search Space가 2 이상 필요.
+// binarySearch3: 세개의 element(왼쪽, 가운데, 오른쪽)에 접근하여 정답을 얻을 수 있는 경우 사용. Search Space가 3 이상 필요.
+
+int binarySearch1(vector<int>& nums, int target){
   if(nums.size() == 0)
     return -1;
 
@@ -13,4 +17,47 @@ int binarySearch(vector<int>& nums, int target){
 
   // End Condition: left > right
   return -1;
+}
+
+int binarySearch2(vector<int>& nums, int target){
+  if(nums.size() == 0)
+    return -1;
+
+  int left = 0, right = nums.size();
+  while(left < right){
+    // Prevent (left + right) overflow
+    int mid = left + (right - left) / 2;
+    if(nums[mid] == target){ return mid; }
+    else if(nums[mid] < target) { left = mid + 1; }
+    else { right = mid; }
+  }
+
+  // Post-processing:
+  // End Condition: left == right
+  if(left != nums.size() && nums[left] == target) return left;
+  return -1;
+}
+
+int binarySearch3(vector<int>& nums, int target){
+    if (nums.size() == 0)
+        return -1;
+
+    int left = 0, right = nums.size() - 1;
+    while (left + 1 < right){
+        // Prevent (left + right) overflow
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+    // Post-processing:
+    // End Condition: left + 1 == right
+    if(nums[left] == target) return left;
+    if(nums[right] == target) return right;
+    return -1;
 }
