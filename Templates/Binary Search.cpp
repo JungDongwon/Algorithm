4a8fb720e920e66d83aa01211da8415b81653da9
Jung Dongwon <dg52316@naver.com>
2022-04-30 13:08:31 +0900

    Update Binary Search.cpp

--- Templates/Binary Search.cpp
+++ Templates/Binary Search.cpp
@@ -1,5 +1,4 @@
 // binarySearch1: 가장 기본적인 형태, 한 개의 element만 접근하여 정답을 얻을 수 있는 경우, Duplicate이 없는 경우
-// binarySearch2: 한 개의 element만 접근하여 정답을 얻을 수 없는 경우, Duplicate이 있는 경우, 마지막에 한 개의 element만 남음
 // binarySearch3: 한 개의 element만 접근하여 정답을 얻을 수 없는 경우, Duplicate이 있는 경우, 마지막에 두 개의 element만 남음
 
 int binarySearch1(vector<int>& nums, int target){
@@ -16,25 +15,6 @@ int binarySearch1(vector<int>& nums, int target){
   }
 
   // End Condition: left > right
-  return -1;
-}
-
-int binarySearch2(vector<int>& nums, int target){
-  if(nums.size() == 0)
-    return -1;
-
-  int left = 0, right = nums.size();
-  while(left < right){
-    // Prevent (left + right) overflow
-    int mid = left + (right - left) / 2;
-    if(nums[mid] == target){ return mid; }
-    else if(nums[mid] < target) { left = mid + 1; }
-    else { right = mid; }
-  }
-
-  // Post-processing:
-  // End Condition: left == right
-  if(left != nums.size() && nums[left] == target) return left;
   return -1;
 }
 
