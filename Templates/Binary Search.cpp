3e8d49a511c0f0df9ad87dded189c8ced2fcf312
Jung Dongwon <dg52316@naver.com>
2022-04-30 13:37:09 +0900

    Update Binary Search.cpp

--- Templates/Binary Search.cpp
+++ Templates/Binary Search.cpp
@@ -1,43 +1,20 @@
-// binarySearch1: 가장 기본적인 형태, 한 개의 element만 접근하여 정답을 얻을 수 있는 경우, Duplicate이 없는 경우
-// binarySearch3: 한 개의 element만 접근하여 정답을 얻을 수 없는 경우, Duplicate이 있는 경우, 마지막에 두 개의 element만 남음
-
-int binarySearch1(vector<int>& nums, int target){
+int binarySearch(vector<int>& nums, int target){
   if(nums.size() == 0)
     return -1;
 
-  int left = 0, right = nums.size() - 1;
-  while(left <= right){
-    // Prevent (left + right) overflow
+  int left = 0, right = nums.size() - 1; // Consideration 1 : left, right 범위
+  
+  while(left < right){
     int mid = left + (right - left) / 2;
-    if(nums[mid] == target){ return mid; }
-    else if(nums[mid] < target) { left = mid + 1; }
-    else { right = mid - 1; }
+    
+    if (condition(mid)) { right = mid; } // Consideration 3 : condition이 무엇인가?
+    else { left = mid + 1}
   }
 
-  // End Condition: left > right
-  return -1;
+  return left; // Consideration 2 : 문제에서 원하는 정답이 무엇인가? (ex. left, left-1)
 }
 
-int binarySearch3(vector<int>& nums, int target){
-    if (nums.size() == 0)
-        return -1;
-
-    int left = 0, right = nums.size() - 1;
-    while (left + 1 < right){
-        // Prevent (left + right) overflow
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid;
-        } else {
-            right = mid;
-        }
-    }
-
-    // Post-processing:
-    // End Condition: left + 1 == right
-    if(nums[left] == target) return left;
-    if(nums[right] == target) return right;
-    return -1;
-}
+// * Solutions for Considerations... 
+// Consideration 1 : Set up the boundary to include all possible elements
+// Consideration 2 : After exiting the while loop, left is the minimal k satisfying the condition function
+// Consideration 3 : Experiences
