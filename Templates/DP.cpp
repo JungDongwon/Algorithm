8f709f5073845aab66a855694f9587dfc462515d
Jung Dongwon <dg52316@naver.com>
2022-05-24 13:32:05 +0900

    Update and rename Dynamic Programming.cpp to DP.cpp

---
+++ Templates/DP.cpp
@@ -0,0 +1,7 @@
+// How to determine it is a Dynamic Programming problems
+// 1. The problem will ask for the optimum value (maximum or minimum) of something
+// 2. Future "decisions" depend on earlier decisions. If it isn't, it can be an greedy algorithm.
+// ** When trying to decide if the second characteristic is applicable, 
+//    assume it isn't, then try to think of a counterexample that proves a greedy algorithm won't work. **
+
+// reference: LeetCode
--- Templates/Dynamic Programming.cpp
+++
@@ -1,5 +0,0 @@
-// How to determine it is a Dynamic Programming problems
-// 1. The problem will ask for the optimum value (maximum or minimum) of something
-// 2. Future "decisions" depend on earlier decisions.
-
-// reference: LeetCode
